53882e7496be3b95afbeffad6f821195ceb0adc3
Joakim Larsson <joakim.larsson@omegapoint.se>
2020-10-22 15:24:02 +0200

    Add Material Design and Tasks to complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'myAngularProject';
+  items = [
+    'Logga in/ut med sin användare',
+    'Updatera sin användare',
+    'Ladda upp filer och hämta hem dem.',
+    'Se filerna man har under sitt konto.',
+    'Enhetstester för skriven kod.',
+    'Versionshanterat',
+    'Avgräsning: Fokus är angular, ingen backend/DB är nödvändigt.',
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { NgxFileDropModule } from 'ngx-file-drop';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FileUploadComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    NgxFileDropModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
