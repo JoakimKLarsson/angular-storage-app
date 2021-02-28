b4bb58ce77db16ba299373d43870cc76d1031c7b
Joakim Larsson <joakim.larsson@omegapoint.se>
2021-02-28 14:21:16 +0100

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   moduleNameMapper: {
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
+    "@core/(.*)": "<rootDir>/src/app/core/$1",
   },
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  coverageReporters: ["json", "html"],
+  coverageDirectory: "./coverage",
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
 };
